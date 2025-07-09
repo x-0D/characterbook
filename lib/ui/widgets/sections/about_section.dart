0cb755197bab459f36d9f365af41d539ac2187fb
MaxGog <max.gog2005@outlook.com>
2025-07-09 12:53:10 +0300

    Обновление версии

--- lib/ui/widgets/sections/about_section.dart
+++ lib/ui/widgets/sections/about_section.dart
@@ -36,7 +36,7 @@ class AboutSection extends StatelessWidget {
           leading: const Icon(Icons.info_outline),
           title: Text(s.version),
           trailing: Text(
-            '1.6.5',
+            '1.6.6',
             style: Theme.of(context).textTheme.bodyLarge,
           ),
         ),
