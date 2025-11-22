26e0809fcab9c94689ff348efd05a1cf98fa2b9f
MaxGog <max.gog2005@outlook.com>
2025-11-22 14:04:54 +0300

    Обновление версии в разделе "Лицензия"

--- lib/ui/widgets/sections/licenses_section.dart
+++ lib/ui/widgets/sections/licenses_section.dart
@@ -31,7 +31,7 @@ class LicensesSection extends StatelessWidget {
           onTap: () => showLicensePage(
             context: context,
             applicationName: s.app_name,
-            applicationVersion: '1.6.8',
+            applicationVersion: '1.7.0',
             applicationIcon: Image.asset(
               'assets/iconapp.png',
               width: 50,
